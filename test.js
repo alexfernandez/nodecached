4f85040e7f8db8c9a7abacde9ae019fca62ec3a7
Alex Fernández <alex.fernandez@mediasmart.es>
2013-11-05 01:27:16 +0100

    Added memory cache.

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ exports.test = function(callback)
 {
 	log.debug('Running tests');
 	var tests = {};
-	var modules = ['server'];
+	var modules = ['server', 'memory'];
 	modules.forEach(function(name)
 	{
 		var filename = './lib/' + name + '.js';
