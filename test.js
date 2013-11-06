d93add2fa394874e63181b072147947f6daf3ce0
Alex Fernández <alex.fernandez@mediasmart.es>
2013-11-07 00:42:48 +0100

    Added a new module parser.js that parses line by line.

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ exports.test = function(callback)
 {
 	log.debug('Running tests');
 	var tests = {};
-	var modules = ['server', 'memory', 'commands'];
+	var modules = ['server', 'memory', 'commands', ' parser'];
 	modules.forEach(function(name)
 	{
 		var filename = './lib/' + name + '.js';
