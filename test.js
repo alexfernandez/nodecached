84183cbddede3a5b839f12bb77959c5bf9b60823
Alex Fernández <alex.fernandez@mediasmart.es>
2013-11-10 00:27:47 +0100

    Added client tests.

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ exports.test = function(callback)
 {
 	log.debug('Running tests');
 	var tests = {};
-	var modules = ['server', 'memory', 'commands', 'parser', 'token'];
+	var modules = ['server', 'memory', 'commands', 'parser', 'client', 'token'];
 	modules.forEach(function(name)
 	{
 		var filename = './lib/' + name + '.js';
