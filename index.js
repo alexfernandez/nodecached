ec51eb425672976f71b6c4f1b48144ef4ced1aa3
Alex Fernández <alex.fernandez@mediasmart.es>
2013-12-01 22:37:04 +0100

    Export Client and Cache from index.js.

--- index.js
+++ index.js
@@ -8,8 +8,12 @@
 
 // requires
 var server = require('./lib/server.js');
+var client = require('./lib/client.js');
+var memory = require('./lib/memory.js');
 
 // exports
 exports.start = server.start;
+exports.Client = client.Client;
+exports.Cache = memory.Cache;
 
 
